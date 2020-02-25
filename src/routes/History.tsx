f664364a6a02e4e7509c8bc4c10caaf2fb429d9c
Lazar Szilard <lazar.szilard1995@gmail.com>
2020-02-25 20:47:48 +0200

    History page wip

--- src/routes/History.tsx
+++ src/routes/History.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Col, Row, Nav, Tab } from 'react-bootstrap';
+import styled from 'styled-components';
+import CardMap from './../components/external/CardMap';
+import { ICard } from './../components/external/YuGiOhCard';
+import { cards } from './../FakeApi';
 
 export interface IListItem
 {
     eventKey: string,
-    text: string
+    text: string,
+    type: string
 }
 
 const hidden_sidebar = () => {
@@ -20,43 +26,92 @@ const show_sidebar = () => {
   }
 }
 
+const filterCards = (cards: ICard[], type: string) => {
+  if (type === "all") return cards;
+  return cards.filter(card => card.type === type);
+}
+
+const ListItem = styled.div `
+
+`;
+
 function History() {
   let lateralBar: IListItem[] = [];
+  let all_cards = cards;
   lateralBar = [
     {
-      eventKey: "zero_item",
-      text: "| | |"
+      eventKey: "all_card",
+      text: "All Card",
+      type: "all"
     },
     {
-      eventKey: "first_item",
-      text: "All Card"
+      eventKey: "exodia_pieces",
+      text: "Exodia Pieces",
+      type: "exodia"
     },
     {
-      eventKey: "second_item",
-      text: "Trap Cards"
+      eventKey: "trap_cards",
+      text: "Trap Cards",
+      type: "trap"
     },
     {
-      eventKey: "third_item",
-      text: "Magic Cards"
+      eventKey: "magic_cards",
+      text: "Magic Cards",
+      type: "magic"
     },
     {
-      eventKey: "fourth_item",
-      text: "Monster Cards"
+      eventKey: "monster_cards",
+      text: "Monster Cards",
+      type: "monster"
     },
-  ]
-  
+    {
+      eventKey: "spell_card",
+      text: "Spell Cards",
+      type: "spell"
+    },
+  ];
+
   return (
     <div>
-       <div className="w3-sidebar w3-bar-block w3-border-right" style={{display: "block"}} id="mySidebar">
-          <button onClick={() => hidden_sidebar()} className="w3-bar-item w3-large">Close &times;</button>
-          <a href="/cards" className="w3-bar-item w3-button">Link 1</a>
-          <a href="/" className="w3-bar-item w3-button">Link 2</a>
-          <a href="/cards" className="w3-bar-item w3-button">Link 3</a>
-      </div>
+      <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+        <Row>
+          <Col sm={2}>
+            <div className="w3-sidebar w3-bar-block w3-border-right" style={{display: "block"}} id="mySidebar">
+              <button onClick={() => hidden_sidebar()} className="w3-bar-item w3-large">Close &times;</button>
+              <Nav variant="pills" className="flex-column">
+              {
+                lateralBar.map(item => {
+                  return(
+                    <Nav.Item>
+                      <Nav.Link eventKey={item.eventKey}>
+                        <ListItem>
+                          { item.text } 
+                        </ListItem>
+                      </Nav.Link>
+                    </Nav.Item>
+                  )
+                })
+              }
+              </Nav>
+            </div>
 
-      <div className="w3-teal">
-        <button className="w3-button w3-teal w3-xlarge" onClick={(e) => show_sidebar()}>☰</button>
-      </div>
+            <div>
+              <button className="w3-button w3-xlarge" onClick={(e) => show_sidebar()}>☰</button>
+            </div>
+          </Col>
+          <Col>
+            <Tab.Content>
+            {
+              lateralBar.map(item => 
+                <Tab.Pane eventKey={ item.eventKey }>
+                  <CardMap cards={ filterCards(all_cards, item.type) }/>
+                </Tab.Pane>
+              )
+            }
+            </Tab.Content>
+          </Col>
+        </Row>
+      </Tab.Container>
     </div>
   );
 }
